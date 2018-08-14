c95f89fa2aa5af1d20ce1553769e7640db0c8ce0
Useit015 <useit015@gmail.com>
2018-08-14 01:39:21 +0100

    changed paths

--- gulpfile.js
+++ gulpfile.js
@@ -5,17 +5,25 @@ const sass = require('gulp-sass');
 // Compile Sass and inject into browser
 gulp.task('sass', function() {
   return gulp
-    .src(['src/base.scss'])
+    .src(['src/css/base.scss'])
     .pipe(sass())
-    .pipe(gulp.dest('src/'))
+    .pipe(gulp.dest('src/css/'))
+    .pipe(browserSync.stream());
+});
+
+// Move JS files to src/js
+gulp.task('js', function() {
+  return gulp
+    .src(['node_modules/vue/dist/vue.min.js'])
+    .pipe(gulp.dest('src/js/'))
     .pipe(browserSync.stream());
 });
 
 // Watch Files
-gulp.task('default', ['sass'], function() {
+gulp.task('default', ['sass', 'js'], function() {
   browserSync.init({
     server: '.'
   });
-  gulp.watch(['src/base.scss'], ['sass']);
-  gulp.watch('src/index.html').on('change', browserSync.reload);
+  gulp.watch(['src/css/base.scss'], ['sass']);
+  gulp.watch('index.html').on('change', browserSync.reload);
 });
